9dbc65590e3381d6c2f4acf142b1878d12b410e2
Michal-Atlas <michal.z.atlas@gmail.com>
2020-08-06 03:37:30 +0200

    Added Races Enum

---
+++ character/race/races.cpp
@@ -0,0 +1,11 @@
+#include "races.h"
+#include "../character.h"
+
+namespace DawnStorm {
+    void Character::Choose_Race(Race choice) {
+        switch () {
+
+        }
+    }
+}
+
---
+++ character/race/races.h
@@ -0,0 +1,16 @@
+#ifndef STORM_ENGINE_RACES_H
+#define STORM_ENGINE_RACES_H
+
+namespace DawnStorm {
+    enum Race {
+        Human,
+        Goblin,
+        Isk,
+        Keinfolk,
+        Talach,
+        Talkin,
+        Tylluan
+    };
+}
+
+#endif //STORM_ENGINE_RACES_H
