1ba0e527c52c5d95309540ea94648dedb3c1a4a4
Michal-Atlas <michal.z.atlas@gmail.com>
2020-08-07 01:30:13 +0200

    Added NPC with Dialogue Component

---
+++ characters/npc/dialogue_component.h
@@ -0,0 +1,10 @@
+#ifndef STORMENGINE_DIALOGUE_COMPONENT_H
+#define STORMENGINE_DIALOGUE_COMPONENT_H
+
+namespace DawnStorm {
+    class Dialogue_Component {
+
+    };
+}
+
+#endif //STORMENGINE_DIALOGUE_COMPONENT_H
---
+++ characters/npc/npc.h
@@ -0,0 +1,14 @@
+#ifndef STORMENGINE_NPC_H
+#define STORMENGINE_NPC_H
+
+#include "../../ai/ai.h"
+#include "dialogue_component.h"
+
+namespace DawnStorm {
+    class NPC : AI, Assailable {
+    public:
+        Dialogue_Component dialogue;
+    };
+}
+
+#endif //STORMENGINE_NPC_H
